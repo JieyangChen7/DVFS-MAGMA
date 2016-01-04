9251b5869418e7f95a235bf42ab391a168906870
Jieyang Chen <cjy7117@gmail.com>
2016-01-04 13:40:41 -0800

    test

--- test/magma-1.6.1_timing/scripts/MAGMA_LU_GPUDVFS_ivy.sh
+++ test/magma-1.6.1_timing/scripts/MAGMA_LU_GPUDVFS_ivy.sh
@@ -22,7 +22,7 @@ rapl_pid=$!
 #time /home/lchen/MAGMA/magma-1.6.1/testing/testing_dgetrf -N 3000,3000 -c
 #sudo time /home/lchen/MAGMA/magma-1.6.1/testing/testing_dgetrf -N 20000,20000
 
-sudo LD_LIBRARY = $LD_LIBRARY time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 5120,5120
+sudo LD_LIBRARY_PATH = $LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 5120,5120
 #ssh -t -t ivy sudo time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 10240,10240
 #ssh -t -t ivy sudo time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 15360,15360
 #ssh -t -t ivy sudo time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 20480,20480
