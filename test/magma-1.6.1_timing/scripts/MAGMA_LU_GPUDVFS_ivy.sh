61ddd59422736576db8224e674df0f687d91ac99
Jieyang Chen <cjy7117@gmail.com>
2016-01-05 15:28:26 -0800

    test energy

--- test/magma-1.6.1_timing/scripts/MAGMA_LU_GPUDVFS_ivy.sh
+++ test/magma-1.6.1_timing/scripts/MAGMA_LU_GPUDVFS_ivy.sh
@@ -15,7 +15,6 @@ nvidia-smi -q -d POWER -lms 100 -f /home/lchen/MAGMA/test/magma-1.6.1_timing/res
 smi_pid=$!
 #echo $smi_pid
 /usr/local/bin/rapl -c 0,10 -f /home/lchen/MAGMA/test/magma-1.6.1_timing/results/${case}.cpu.pwr 2>&1 1>/dev/null &
-
 rapl_pid=$!
 #echo $rapl_pid
 
@@ -23,10 +22,10 @@ rapl_pid=$!
 #sudo time /home/lchen/MAGMA/magma-1.6.1/testing/testing_dgetrf -N 20000,20000
 
 sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 5120,5120
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 10240,10240
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 15360,15360
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 20480,20480
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 25600,25600
+#sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 10240,10240
+#sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 15360,15360
+#sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 20480,20480
+#sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dpotrf -N 25600,25600
 
 
 #sudo time /home/lchen/MAGMA/test/magma-1.6.1_timing/testing/testing_dgetrf -N 5120,5120
