18aee587b0f7c388d5f224e85232c0618a684555
Jieyang Chen <cjy7117@gmail.com>
2016-01-20 12:15:45 -0800

    test

--- magma-1.6.1/src/dgetrf.cpp
+++ magma-1.6.1/src/dgetrf.cpp
@@ -293,18 +293,29 @@ magma_dgetrf(
                                         stream[0]);
                 
 
-//                
-//                if(ALGORITHMIC_SLACK_PREDICTION || GPU_SLACK_RECLAMATION)
-//				{
-//					ratio_slack_pred = 1.0 - (double)nb/(m-j*nb);
-//					cpu_time_pred = cpu_time_pred * ratio_slack_pred;
-//					gpu_time_pred = gpu_time_pred * ratio_slack_pred * ratio_slack_pred;
-//					 
-//					printf("iter %d: cpu_time_pred = %f\n", j, cpu_time_pred);
-//					printf("iter %d: gpu_time_pred = %f\n", j, gpu_time_pred);
-//					printf("iter %d: slack_pred = %f\n", j, cpu_time_pred - gpu_time_pred);
-//				}
-//                
+                
+                if(ALGORITHMIC_SLACK_PREDICTION || GPU_SLACK_RECLAMATION)
+				{
+					ratio_slack_pred = 1.0 - (double)nb/(m-j*nb);
+					cpu_time_pred = cpu_time_pred * ratio_slack_pred;
+					gpu_time_pred = gpu_time_pred * ratio_slack_pred * ratio_slack_pred;
+					 
+					printf("iter %d: cpu_time_pred = %f\n", j, cpu_time_pred);
+					printf("iter %d: gpu_time_pred = %f\n", j, gpu_time_pred);
+					printf("iter %d: slack_pred = %f\n", j, cpu_time_pred - gpu_time_pred);
+				}
+                if (j == 1) {
+                	cpu_time_pred = 0.060726;
+					gpu_time_pred = 0.030097;
+                } else {
+                	ratio_slack_pred = 1.0 - (double)nb/(m-j*nb);
+					cpu_time_pred = cpu_time_pred * ratio_slack_pred;
+					gpu_time_pred = gpu_time_pred * ratio_slack_pred * ratio_slack_pred;
+					 
+					printf("iter %d: cpu_time_pred = %f\n", j, cpu_time_pred);
+					printf("iter %d: gpu_time_pred = %f\n", j, gpu_time_pred);
+                }
+                
                 
                 if(GPU_SLACK_RECLAMATION)
 				{
