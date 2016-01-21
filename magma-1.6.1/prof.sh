2e9173f5ee67a3aa39ee5791e2dc4d67c0a989d8
Jieyang Chen <cjy7117@gmail.com>
2016-01-20 22:19:12 -0800

    test

--- magma-1.6.1/prof.sh
+++ magma-1.6.1/prof.sh
@@ -1,4 +1,4 @@
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /usr/local/cuda-7.0/bin/nvprof --profile-from-start off --print-gpu-trace -o profile ./testing/testing_dgetrf -N 20480,20480
+sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /usr/local/cuda-7.0/bin/nvprof --profile-from-start off --print-gpu-trace -o profile ./testing/testing_dgetrf -N 15360,15360
 git add profile
 git commit -m "updated profile"
 git push origin master
