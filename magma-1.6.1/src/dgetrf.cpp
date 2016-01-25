38c599f60d9fbf01639508399ea487367eee8a49
Jieyang Chen <cjy7117@gmail.com>
2016-01-25 14:49:56 -0800

    test

--- magma-1.6.1/src/dgetrf.cpp
+++ magma-1.6.1/src/dgetrf.cpp
@@ -108,7 +108,7 @@ magma_dgetrf(
     /* Function Body */
     //nb = magma_get_dgetrf_nb(m);
     //cout << "nb="<<nb<<endl; 
-    nb = 101;//optimal
+    nb = 105;//optimal
 
     if ( (nb <= 1) || (nb >= min(m,n)) ) {
         /* Use CPU code. */
