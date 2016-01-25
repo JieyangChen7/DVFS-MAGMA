7eb02129e68292c1c70872b808c9ae661345bdb7
Jieyang Chen <cjy7117@gmail.com>
2016-01-25 15:55:31 -0800

    test

--- magma-1.6.1/testing/testing_dgetrf.cpp
+++ magma-1.6.1/testing/testing_dgetrf.cpp
@@ -230,7 +230,7 @@ int main( int argc, char** argv)
             printf("init_time = %.6f\n", init_time);
 
             //SetGPUFreq(324, 324);
-            SetGPUFreq(2600, 705);
+            SetGPUFreq(2600, 614);
 
             gpu_time = magma_wtime();
             
