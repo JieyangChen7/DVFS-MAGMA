0d607c6d7b9cb34d41886cc65468f5a9f5bd774f
Jieyang Chen <cjy7117@gmail.com>
2016-01-28 14:58:10 -0800

    test

--- magma-1.6.1/src/dgetrf.cpp
+++ magma-1.6.1/src/dgetrf.cpp
@@ -382,7 +382,7 @@ magma_dgetrf(
                 }
                 
                 
-                if(j > 1 && j < 30)
+                if(j > 1 && j < 20)
 				{
 					ratio_split_freq = (cpu_time_pred - gpu_time_pred) / (gpu_time_pred * ((gpu_time_iter0_lowest_freq / gpu_time_iter0_highest_freq) - 1));
 					//printf("iter %d: ratio_split_freq = %f\n", j, ratio_split_freq);
