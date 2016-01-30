ea8613562312cc4dda07d8794603aeb0abbb800a
Jieyang Chen <cjy7117@gmail.com>
2016-01-29 21:25:18 -0800

    test

--- magma-1.6.1/scripts/MAGMA_LU_GPUDVFS_ivy.sh
+++ magma-1.6.1/scripts/MAGMA_LU_GPUDVFS_ivy.sh
@@ -8,8 +8,8 @@ case=MAGMA_LU_GPUDVFS
 #/home/lchen/cpu_l_ivy.sh
 #sleep 9
 
-/opt/power-bench/mclient -H 130.127.48.185 -d /home/lchen/MAGMA/test/magma-1.6.1_timing/results
-/opt/power-bench/mclient -H 130.127.48.185 -l ${case}.total.pwr
+/opt/power-bench/mclient -H 172.16.10.55 -d /home/lchen/MAGMA/test/magma-1.6.1_timing/results
+/opt/power-bench/mclient -H 172.16.10.55 -l ${case}.total.pwr
 
 nvidia-smi -q -d POWER -lms 100 -f /home/lchen/MAGMA/magma-1.6.1/results/${case}.gpu.pwr 2>&1 1>/dev/null &
 smi_pid=$!
@@ -19,7 +19,7 @@ rapl_pid=$!
 #echo $rapl_pid
 
 
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /home/lchen/MAGMA/magma-1.6.1/testing/testing_dgetrf -N 5120,5120
+sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /home/lchen/MAGMA/magma-1.6.1/testing/testing_dpotrf -N 5120,5120
 
 
 #time /home/lchen/MAGMA/magma-1.6.1/testing/testing_dgetrf -N 3000,3000 -c
@@ -43,7 +43,7 @@ sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /home/lchen/MAGMA/magma-1.6.1/testing/
 #ssh -t -t ivy sudo time /home/lchen/MAGMA/magma-1.6.1/testing/testing_dgeqrf -N 20480,20480
 #ssh -t -t ivy sudo time /home/lchen/MAGMA/magma-1.6.1/testing/testing_dgeqrf -N 25600,25600
 
-/opt/power-bench/mclient -H 130.127.48.185 -e log
+/opt/power-bench/mclient -H 172.16.10.55 -e log
 
 kill ${smi_pid}
 kill ${rapl_pid}
