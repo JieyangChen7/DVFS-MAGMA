16d818a98ee27193c76c9b73a4ed5fbab26c70bc
Jieyang Chen <cjy7117@gmail.com>
2016-01-29 22:16:48 -0800

    test

--- magma-1.6.1/testing/testing_dpotrf.cpp
+++ magma-1.6.1/testing/testing_dpotrf.cpp
@@ -85,7 +85,7 @@ int main( int argc, char** argv)
                =================================================================== */
             //SetGPUFreq(324, 324);
             SetGPUFreq(2600, 705);
-            SetCPUFreq(1600000);
+            SetCPUFreq(1700000);
 
             gpu_time = magma_wtime();
             magma_dpotrf( opts.uplo, N, h_R, lda, &info );
