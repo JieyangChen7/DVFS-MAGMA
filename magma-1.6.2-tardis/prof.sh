10a8b589f041fe64efda515acf437e1f08e8ffab
Jieyang Chen <cjy7117@gmail.com>
2016-04-03 15:47:50 -0700

    test

--- magma-1.6.2-tardis/prof.sh
+++ magma-1.6.2-tardis/prof.sh
@@ -1,4 +1,5 @@
-nvprof --profile-from-start off --print-gpu-trace -of profile ./testing/testing_dgeqrf -N 20480,20480
+rm profile
+nvprof --profile-from-start off --print-gpu-trace -o profile ./testing/testing_dgeqrf -N 20480,20480
 git add profile
 git commit -m "updated profile"
 git push origin master
