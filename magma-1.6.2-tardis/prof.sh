54898460cecd4983f79a704c4abc2c3b4bdcceff
Jieyang Chen <cjy7117@gmail.com>
2016-04-05 15:11:05 -0700

    test

--- magma-1.6.2-tardis/prof.sh
+++ magma-1.6.2-tardis/prof.sh
@@ -1,5 +1,5 @@
 rm profile
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /usr/local/cuda-7.0/bin/nvprof --profile-from-start off --print-gpu-trace -o profile ./testing/testing_dgeqrf -N 20480,20480
+sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /usr/local/cuda-7.0/bin/nvprof --profile-from-start off --print-gpu-trace -o /home/lchen/MAGMA/magma-1.6.2-tardis/profile ./testing/testing_dgeqrf -N 20480,20480
 
 git add profile
 git commit -m "updated profile"
