7ca0f7bc6b2948124e96a1f406e2b455b83efb4b
Jieyang Chen <cjy7117@gmail.com>
2016-04-05 15:40:02 -0700

    test

--- magma-1.6.2-tardis/prof.sh
+++ magma-1.6.2-tardis/prof.sh
@@ -1,4 +1,6 @@
 rm profile
+touch profile
+chmod 777 profile
 sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH env PATH=$PATH nvprof --profile-from-start off --print-gpu-trace -o profile ./testing/testing_dgeqrf -N 20480,20480
 
 git add profile
