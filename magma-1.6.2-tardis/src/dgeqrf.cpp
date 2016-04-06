b86f47a4ff3822aacf25cad34aa4e3929960d37b
Jieyang Chen <cjy7117@gmail.com>
2016-04-06 10:57:09 -0700

    test

--- magma-1.6.2-tardis/src/dgeqrf.cpp
+++ magma-1.6.2-tardis/src/dgeqrf.cpp
@@ -335,6 +335,48 @@ if (timing) {
                     cudaEventDestroy(stop_dvfs);
                     printf("iter:%d dvfs time:%f\n", iter, dvfs_time);
 
+                    cudaEventCreate(&start_dvfs);
+                    cudaEventCreate(&stop_dvfs);
+                    cudaEventRecord(start_dvfs, 0);
+                    SetGPUFreq(324, 324);
+                    
+                    //end gpu timing
+                    cudaEventRecord(stop_dvfs, 0);
+                    cudaEventSynchronize(stop_dvfs);
+                    cudaEventElapsedTime(&dvfs_time, start_dvfs, stop_dvfs);
+                    cudaEventDestroy(start_dvfs);
+                    cudaEventDestroy(stop_dvfs);
+                    printf("iter:%d dvfs time:%f\n", iter, dvfs_time);
+
+                    //start gpu timing
+                    cudaEventCreate(&start_dvfs);
+                    cudaEventCreate(&stop_dvfs);
+                    cudaEventRecord(start_dvfs, 0);
+                    SetGPUFreq(2600, 705);
+                    
+                    //end gpu timing
+                    cudaEventRecord(stop_dvfs, 0);
+                    cudaEventSynchronize(stop_dvfs);
+                    cudaEventElapsedTime(&dvfs_time, start_dvfs, stop_dvfs);
+                    cudaEventDestroy(start_dvfs);
+                    cudaEventDestroy(stop_dvfs);
+                    printf("iter:%d dvfs time:%f\n", iter, dvfs_time);
+
+
+                    //start gpu timing
+                    cudaEventCreate(&start_dvfs);
+                    cudaEventCreate(&stop_dvfs);
+                    cudaEventRecord(start_dvfs, 0);
+                    SetGPUFreq(2600, 705);
+                    
+                    //end gpu timing
+                    cudaEventRecord(stop_dvfs, 0);
+                    cudaEventSynchronize(stop_dvfs);
+                    cudaEventElapsedTime(&dvfs_time, start_dvfs, stop_dvfs);
+                    cudaEventDestroy(start_dvfs);
+                    cudaEventDestroy(stop_dvfs);
+                    printf("iter:%d dvfs time:%f\n", iter, dvfs_time);
+
 
                 }
 
