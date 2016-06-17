d990a2ff2cca57fe0e0de6faf802dda19f05aca2
Jieyang Chen <cjy7117@gmail.com>
2016-06-17 12:04:09 -0700

    test

--- magma-2.0.2/DVFS_testing/MAGMA_LU_GPUDVFS_ivy.sh
+++ magma-2.0.2/DVFS_testing/MAGMA_LU_GPUDVFS_ivy.sh
@@ -23,17 +23,17 @@ rapl_pid=$!
 #echo $rapl_pid
 
 
-sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /home/lchen/MAGMA/magma-2.0.2/testing/testing_dgeqrf -N 20480,20480
+#sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /home/lchen/MAGMA/magma-2.0.2/testing/testing_dgeqrf -N 20480,20480
 
 
-#rm profile
-#touch profile
-#chmod 777 profile
-#sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH env PATH=$PATH nvprof --profile-from-start off --print-gpu-trace -o profile ./testing/testing_dgeqrf -N 20480,20480
+rm profile
+touch profile
+chmod 777 profile
+sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH env PATH=$PATH nvprof --profile-from-start off --print-gpu-trace -o profile ./testing/testing_dgeqrf -N 20480,20480
 
-#git add profile
-#git commit -m "updated profile"
-#git push origin master
+git add profile
+git commit -m "updated profile"
+git push origin master
 
 
 #sudo env LD_LIBRARY_PATH=$LD_LIBRARY_PATH /home/lchen/MAGMA/magma-1.6.2-tardis/testing/testing_dgeqrf -N 15360,15360
